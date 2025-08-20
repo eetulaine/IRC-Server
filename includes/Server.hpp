ca6be1b1e865c4fcdbfdec869df50a7d10e23f23
shahnajsc <sschowdhury29@gmail.com>
2025-08-20 11:36:35 +0300

    made corrections in methods for attribute name change and epoll argument

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -17,6 +17,7 @@
 #include "../includes/Client.hpp"
 
 class Client;
+
 class Server {
 
 	private:
@@ -38,7 +39,7 @@ class Server {
 		// dependant methods for "ServerActivity"
 		void acceptNewClient(int epollFd); //-> accept new client request
 		std::string getClientIP(struct sockaddr_in clientSocAddr);
-		void receiveData(int currentFD, int epollFD);
+		void receiveData(int currentFD);
 		void sendData(int currentFD);
 		std::map<int, std::unique_ptr<Client>> clients_;
 		std::pair<std::string, std::vector<std::string>> parseCommand(const std::string& line);
